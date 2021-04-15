e377e9d5550ba609afaadbfd33f764eab5d85b04
Zaharix <qwertykaj@mail.ru>
2021-04-16 00:21:06 +0300

    Class Manager

---
+++ Manager.java
@@ -0,0 +1,42 @@
+package ru.mai.whoWantsToBeAMillioner;
+
+public class Manager {
+
+    /**
+     * основной метод программы
+     */
+    public static void main(String[] args) {
+        System.out.println("Main method");
+    }
+
+    /**
+     * Этот метод начинает игру
+     */
+    public void startGame() {
+        System.out.println("Start game method");
+    }
+
+    /**
+     * Этот метод позволяет присоединить друга для игры вдвоем
+     */
+    public void connectPeople() {
+        System.out.println("Connect people method");
+    }
+
+    /**
+     * Этот метод позволяет перезапустить игру
+     */
+    public void restartGame() {
+        System.out.println("restart game method");
+    }
+
+    /**
+     * Этот метод отправляет приз
+     * @param whoGetsThePrize кому отправить приз
+     */
+    private void sendThePrize(String whoGetsThePrize) {
+        System.out.println("send the prize method");
+    }
+
+
+}
