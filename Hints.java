c80b457510436df9e65bba06f6ce135bc1d00173
Zaharix <qwertykaj@mail.ru>
2021-04-16 00:31:32 +0300

    Class Hints

---
+++ Hints.java
@@ -0,0 +1,42 @@
+package ru.mai.whoWantsToBeAMillioner;
+
+public class Hints {
+
+    /**
+     * Этот метод позволяет использовать подсказку ведущего
+     */
+    public void useleadingHint() {
+        System.out.println("Use leading hint method");
+    }
+
+    /**
+     * Этот метод позволяет использовать подсказку замена вопроса
+     */
+    public String useChangeQuestionHint() {
+        String newQuestion = "";
+        System.out.println("Use leading hint method");
+        return newQuestion;
+    }
+
+    /**
+     * Этот метод позволяет использовать подсказку 50 на 50
+     */
+    public void useFiftyFiftyHint() {
+        System.out.println("use 50:50 hint method");
+    }
+
+    /**
+     * Этот метод позволяет использовать подсказку право на ошибку
+     */
+    public void useOneMistakeRuleHint() {
+        System.out.println("use one mistake rule hint method");
+    }
+
+    /**
+     * Этот метод позволяет использовать подсказку звонок другу
+     */
+    public void useCallToFriendHint() {
+        System.out.println("use call to friend hint method");
+    }
+}
+
--- Main.java
+++
@@ -1,4 +0,0 @@
-package ru.mai.whoWantsToBeAMillioner;
-
-public class Main {
-}
