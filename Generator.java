21dc272df5a53523cd71373fbdbc0118564c8e3d
Zaharix <qwertykaj@mail.ru>
2021-04-16 00:11:23 +0300

    Class Generator

---
+++ Generator.java
@@ -0,0 +1,30 @@
+package ru.mai.whoWantsToBeAMillioner;
+
+public class Generator {
+    /**
+     * Этот метод генерирует первую часть вопросов
+     */
+    public String[] generateFirstPart() {
+        String[] questions = {""};
+        System.out.println("generate first part of questions method");
+        return questions;
+    }
+
+    /**
+     * Этот метод генерирует вторую часть вопросов
+     */
+    public String[] generateSecondPart() {
+        String[] questions = {""};
+        System.out.println("generate second part of questions method");
+        return questions;
+    }
+
+    /**
+     * Этот метод генерирует третью часть вопросов
+     */
+    public String[] generateThirdPart() {
+        String[] questions = {""};
+        System.out.println("generate third part of questions method");
+        return questions;
+    }
+}
