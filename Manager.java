97a8144c052d1bef294aa8f0374780d668a0c07f
Zaharix <qwertykaj@mail.ru>
2021-04-20 13:19:45 +0300

    Names changed

--- Manager.java
+++ Manager.java
@@ -5,7 +5,7 @@ public class Manager {
     /**
      * основной метод программы
      */
-    public void main(String[] args) {
+    public static void  main(String[] args) {
         System.out.println("Main method");
     }
 
