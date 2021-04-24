79dbb2af437aa6a454be239ef9146170d70cdf99
Zaharix <qwertykaj@mail.ru>
2021-04-24 10:02:33 +0300

    Added Main

---
+++ Main.java
@@ -0,0 +1,9 @@
+package ru.mai.whoWantsToBeAMillioner;
+
+public class Main {
+    public static void  main(String[] args) {
+        Manager manager = new Manager();
+        manager.startGame();
+        System.out.println("Main method");
+    }
+}
