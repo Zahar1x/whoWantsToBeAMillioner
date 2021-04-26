96443c4e16ab96610c8cc8fdcab35ce723fecf62
Zaharix <qwertykaj@mail.ru>
2021-04-26 20:22:49 +0300

    Added methods and new java docs

--- Hints.java
+++ Hints.java
@@ -1,44 +1,93 @@
 package ru.mai.whoWantsToBeAMillioner;
 
 public class Hints {
-
-    private int hintType;
+    private int useleadingsHintCounter = 0;
+    private int useChangeQuestionHintCounter = 0;
+    private int useFiftyFiftyHintCounter = 0;
+    private int useOneMistakeRuleHintCounter = 0;
+    private int useCallToFriendHintCounter = 0;
 
     /**
      * Этот метод позволяет использовать подсказку ведущего
      */
     public void useleadingsHint() {
-        System.out.println("Use leading hint method");
+        System.out.println("USE LEADING HINT METHOD");
+        this.useleadingsHintCounter++;
     }
 
     /**
      * Этот метод позволяет использовать подсказку замена вопроса
+     * @return новый вопрос
      */
     public String useChangeQuestionHint() {
-        String newQuestion = "";
-        System.out.println("Use leading hint method");
-        return newQuestion;
+        Generator generator = new Generator();
+        System.out.println("USE LEADING HINT METHOD");
+        this.useChangeQuestionHintCounter++;
+        return generator.extraQuestion();
     }
 
     /**
      * Этот метод позволяет использовать подсказку 50 на 50
      */
     public void useFiftyFiftyHint() {
-        System.out.println("use 50:50 hint method");
+        System.out.println("USE 50:50 HINT METHOD");
+        this.useFiftyFiftyHintCounter++;
     }
 
     /**
      * Этот метод позволяет использовать подсказку право на ошибку
      */
     public void useOneMistakeRuleHint() {
-        System.out.println("use one mistake rule hint method");
+        System.out.println("USE ONE MISTAKE RULE HINT METHOD");
+        this.useOneMistakeRuleHintCounter++;
     }
 
     /**
      * Этот метод позволяет использовать подсказку звонок другу
      */
     public void useCallToFriendHint() {
-        System.out.println("use call to friend hint method");
+        System.out.println("USE CALL TO FRIEND METHOD");
+        this.useCallToFriendHintCounter++;
+    }
+
+    /**
+     * Этот метод позволяет получить количество вызовов подсказки ведущего
+     * @return кол-во подсказок
+     */
+    public int getUseleadingsHintCounter() {
+        return useleadingsHintCounter;
+    }
+
+    /**
+     * Этот метод позволяет получить количество вызовов подсказки замена вопросов
+     * @return кол-во подсказок
+     */
+    public int getUseChangeQuestionHintCounter() {
+        return useChangeQuestionHintCounter;
+    }
+
+    /**
+     * Этот метод позволяет получить количество вызовов подсказки 50 на 50
+     * @return кол-во подсказок
+     */
+    public int getUseFiftyFiftyHintCounter() {
+        return useFiftyFiftyHintCounter;
+    }
+
+    /**
+     * Этот метод позволяет получить количество вызовов подсказки право на ошибку
+     * @return кол-во подсказок
+     */
+    public int getUseOneMistakeRuleHintCounter() {
+        return useOneMistakeRuleHintCounter;
+    }
+
+    /**
+     * Этот метод позволяет получить количество вызовов подсказки звонок другу
+     * @return кол-во подсказок
+     */
+    public int getUseCallToFriendHintCounter() {
+        return useCallToFriendHintCounter;
     }
 }
 
