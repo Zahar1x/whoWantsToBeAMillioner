c605106455fa63c46d74f5f2a657b929e7ca71cf
Zaharix <qwertykaj@mail.ru>
2021-04-26 20:16:06 +0300

    Added methods and new java docs

--- Answers.java
+++ Answers.java
@@ -1,16 +1,21 @@
 package ru.mai.whoWantsToBeAMillioner;
 
+import java.util.ArrayList;
+
 public class Answers {
+
+    /**
+     * Объект класса подсказки
+     */
+    private Hints hint = new Hints();
 
     /**
      * Этот метод определяет размер приза
      * @param lastAnsweredQuestion этот параметр определяет на каком вопросе остановился игрок
-     * @return размер приза
      */
-    private int assignPrize(int lastAnsweredQuestion) {
-        int prizeAmount = 0;
-        System.out.println("Assign prize method");
-        return prizeAmount;
+    public void assignPrize(int lastAnsweredQuestion) {
+        int prizeAmount = lastAnsweredQuestion * 100;
+        System.out.println(prizeAmount);
     }
 
     /**
@@ -19,10 +24,17 @@ public class Answers {
      * @param question вопрос, на который игрок дает ответ
      * @return правильный ответ или нет
      */
-    private boolean checkAnswerAndPrintResult(String question, int numOfAnswer) {
-        boolean bool = false;
+    public void checkAnswerAndPrintResult(String question, int numOfAnswer, int numOfQuestion) {
         System.out.println("check Answer And Print Result method");
-        return bool;
+        Manager manager = new Manager();
+        if (Math.random() < 0.2) {
+            System.out.println("Answer is wrong!\nHere is your prize");
+            assignPrize(numOfQuestion);
+            manager.sendThePrize("Player 1");
+            manager.endGame();
+        } else {
+            System.out.println("Answer is true");
+        }
     }
 
     /**
@@ -34,12 +46,35 @@ public class Answers {
 
     /**
      * Этот метод позволяет игроку выбрать вариант ответа на поставленный вопрос
-     * @param question вопрос, на который игроку нужно ответить
-     * @return номер выбранного варианта ответа
+     * @param questionList Список вопросов и ответов к ним
+     * @param numOfQuestion номер вопроса, который задан игроку
      */
-    public int chooseAnswer(String question) {
-        int chosenAnswer = 0;
+    public int chooseAnswer(ArrayList questionList, int numOfQuestion) {
         System.out.println("Choose answer method");
+        useHintOrNo(hint);
+        int chosenAnswer = 1;
+        acceptAnswer();
         return chosenAnswer;
+    }
+
+    /**
+     *Этот метод спршивает игрока будет ли он использовать подсказку к данному вопросу и позволяет ему использовать ее
+     * @param hint объект класса Hints, дает доступ к различным подсказкам
+     */
+    public void useHintOrNo(Hints hint) {
+        double rand = Math.random();
+        if (rand < 0.5) {
+            if (rand < 0.1 && hint.getUseCallToFriendHintCounter() < 1) {
+                hint.useCallToFriendHint();
+            } else if (rand < 0.2 && rand > 0.1 && hint.getUseChangeQuestionHintCounter() < 1) {
+                hint.useChangeQuestionHint();
+            } else if (rand < 0.3 && rand > 0.2 && hint.getUseleadingsHintCounter() < 1) {
+                hint.useleadingsHint();
+            } else if (rand < 0.4 && rand > 0.3 && hint.getUseFiftyFiftyHintCounter() < 1) {
+                hint.useFiftyFiftyHint();
+            } else if (rand < 0.5 && rand > 0.4 && hint.getUseOneMistakeRuleHintCounter() < 1) {
+                hint.useOneMistakeRuleHint();
+            }
+        }
     }
 }
